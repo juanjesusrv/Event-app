56af7f3a7ff528a65b33a3acc295f18a1cbfb7fd
DreddSoft <abonilla2093@gmail.com>
2025-02-03 18:19:26 +0100

    Actualización de estilos y nuevo contenido FRONTEND

---
+++ header.php
@@ -0,0 +1,9 @@
+<header>
+    <div class="left">
+        <img src="assets/logo_starbucks.svg" alt="Logo de Starbuck">
+    </div>
+    <div class="right column">
+        <h1>Reserva tu Evento</h1>
+        <span>Evento especial empresa organizado por Starbucks</span>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -3,12 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/logo_starbucks.png" type="image/x-icon">
     <link rel="stylesheet" href="styles.css">
     
-    <title>Document</title>
+    <title>Reserva Evento</title>
 </head>
 <body>
-    <h1>Hola</h1>
+    
+    <?php require_once "header.php" ?>
+    <main>
+
+    <div class="title">
+        <h2>Evento Starbucks</h2>
+        <h3>Entradas</h3>
+    </div>
+    <div class="form-wrapper">
+        <form action="">
+            <!-- nombre, apellidos, email, telefono, fecha, organizacion -->
+             <div class="personal-data">
+                <label for="">Datos Personales</label>
+                <div class="int-data-row">
+                    <input type="text" name="nombre" id="nombre" placeholder="">
+                    <input type="text" name="" id="">
+                </div>
+             </div>
+        </form>
+    </div>
+    </main>
+    <?php require_once "footer.php" ?>
     <script src="scripts.js"></script>
 </body>
 </html>
