30ea491b658840168bc30e1637fa4d20345fce48
DreddSoft <abonilla2093@gmail.com>
2025-02-05 13:06:26 +0100

    Actualización de estilos, formato display a dos bandas con imagen principal promocional

--- header.php
+++ header.php
@@ -1,7 +1,17 @@
 <header>
     <div class="left">
         <img src="assets/logo_starbucks.svg" alt="Logo de Starbuck">
+
+        <nav>
+            <a href="#">CARTA</a>
+            <a href="#">CAFÉ</a>
+            <a href="#">DELIVERY</a>
+            <a href="#">RESPONSABILIDAD</a>
+            <a href="#">MORE </a>
+        </nav>
     </div>
+
+
     <div class="right column">
         <h1>Reserva tu Evento</h1>
         <span>Evento especial empresa organizado por Starbucks</span>
