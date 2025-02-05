024ef4017f5a175de19989d4e6e63f34279635bc
DreddSoft <abonilla2093@gmail.com>
2025-02-05 12:10:44 +0100

    Estructura del formulario creada

--- index.php
+++ index.php
@@ -15,24 +15,64 @@
     <?php require_once "header.php" ?>
     <main>
 
-    <div class="title">
-        <h2>Evento Starbucks</h2>
-        <h3>Entradas</h3>
-    </div>
-    <div class="form-wrapper">
-        <form action="">
-            <!-- nombre, apellidos, email, telefono, fecha, organizacion -->
-             <div class="personal-data">
-                <label for="">Datos Personales</label>
-                <div class="int-data-row">
-                    <input type="text" name="nombre" id="nombre" placeholder="">
-                    <input type="text" name="" id="">
+    <section class="left">
+        <h2>Prueba</h2>
+    </section>
+    <section class="right">
+        <div class="banner">
+            <img src="assets/starbucks_banner.webp" alt="Baner de cafe pistacho">
+        </div>
+
+        <div class="form-wrapper">
+            <form action="">
+                <div class="col">
+                    <label for="name">Nombre</label>
+                    <p class="sub">Nombre del representante</p>
+                    <input type="text" name="name" id="name">
                 </div>
-             </div>
-        </form>
-    </div>
+
+                <div class="col">
+                    <label for="lname">Apellidos</label>
+                    <p class="sub">Apellidos del representante</p>
+                    <input type="text" name="lname" id="lname">
+                </div>
+
+                <div class="col">
+                    <label for="job">Puesto</label>
+                    <p class="sub">Cargo que ocupa</p>
+                    <input type="text" name="job" id="job">
+                </div>
+
+                <div class="col">
+                    <label for="company">Compañia</label>
+                    <div class="sub">Nombre de la empresa</div>
+                    <input type="text" name="company" id="company">
+                </div>
+
+                <div class="col">
+                    <label for="date">Fecha</label>
+                    <input type="date" name="date" id="date">
+                </div>
+
+                <div class="col">
+                    <label for="doc">Documento</label>
+                    <input type="file" name="doc" id="doc">
+                </div>
+
+                <div class="row">
+                    <button type="submit">Enviar</button>
+                </div>
+            </form>
+        </div>
+
+        <div class="cards-wrapper"></div>
+
+            <?php require_once "footer.php" ?>
+    </section>
+
+
     </main>
-    <?php require_once "footer.php" ?>
+
     <script src="scripts.js"></script>
 </body>
 </html>
